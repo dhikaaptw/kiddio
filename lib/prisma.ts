25298ee411ea6ee6f0e6bdd4eefb84dfa122d5e3
Dhika Putra <dhikwrr@gmail.com>
2026-05-09 19:58:12 +0700

    fix: use require for prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { PrismaClient } = require("@prisma/client");
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: any };
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL! }),
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
